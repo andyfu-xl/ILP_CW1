cc0a5ef12ab92dd2393fa66ca2cdfe13b077d398
andyfu-xl <somanyandyfus@gmail.com>
2021-10-05 20:59:23 +0100

    DICE test

--- src/main/java/uk/ac/ed/inf/Const.java
+++ src/main/java/uk/ac/ed/inf/Const.java
@@ -1,23 +1,35 @@
 package uk.ac.ed.inf;
 
+/**
+ *  This class contains only constants will be used in the coursework.
+ */
 public class Const {
     
     // North-End of confinement area has latitude of 55.946233.
-    public static final Double MAXLAT = 55.946233;
+    public static final Double MAX_LAT = 55.946233;
 
     // South-End of confinement area has latitude of 55.842617.
-    public static final Double MINLAT = 55.942617;
+    public static final Double MIN_LAT = 55.942617;
 
     // East-End of confinement area has longitude of -3.184319.
-    public static final Double MAXLONG = -3.184319;
+    public static final Double MAX_LONG = -3.184319;
 
     // West-End of confinement area has longitude of -3.192473.
-    public static final Double MINLONG = -3.192473;
+    public static final Double MIN_LONG = -3.192473;
 
     /* Distance Tolerance of 0.00015 degree, one point is close to another
      if the distance between is strictly less than 0.00015 degree */
-    public static final Double DTOLERANCE = 0.00015;
+    public static final Double DISTANCE_TOLERANCE = 0.00015;
 
     // Distance of each move of drone in degrees
-    public static final Double DMOVE = 0.00015;
+    public static final Double DISTANCE_MOVE = 0.00015;
+
+    // Directory of menus.json in the server
+    public static final String URL_MENUS = "/menus/menus.json";
+
+    // Delivery Charge of 50p
+    public static final int DELIVERY_CHARGE = 50;
+
+    // Angle value when hovering
+    public static final int HOVERING = -999;
 }
--- src/main/java/uk/ac/ed/inf/LongLat.java
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -1,18 +1,18 @@
 package uk.ac.ed.inf;
-import java.lang.Math;
 
 /**
  * representation of a point, by its longitude and latitude.
  */
 public class LongLat {
+
     public Double longitude;
     public Double latitude;
 
     /**
-     * Location constructor
+     * Constructor of LongLat class.
      *
-     * @param lat - Latitude of point on map
-     * @param lng - Longitude of point on map
+     * @param latitude  - Latitude of point on map
+     * @param longitude - Longitude of point on map
      */
     public LongLat(Double longitude, Double latitude) {
         this.longitude = longitude;
@@ -20,47 +20,64 @@ public class LongLat {
     }
 
     /**
-     * check whether the point is within the drone confinement area.
+     * Check whether the point is within the drone confinement area.
+     * The confinement area is a rectangle.
+     *
      * @return true if the point is in the confinement area.
      */
     public boolean isConfined() {
-        boolean validLong = (this.longitude < Const.MAXLONG) & (this.longitude > Const.MINLONG);
-        boolean validLat = (this.latitude < Const.MAXLAT) & (this.latitude > Const.MINLAT);
+        boolean validLong = (this.longitude < Const.MAX_LONG) & (this.longitude > Const.MIN_LONG);
+        boolean validLat = (this.latitude < Const.MAX_LAT) & (this.latitude > Const.MIN_LAT);
         return (validLong & validLat);
     }
 
     /**
      * Apply Pythagorean to calculate distance to another point.
+     *
      * @param point, the target point we want compute its distance from this LongLat.
      * @return a degree (for distance) in double precision.
      */
     public double distanceTo(LongLat point) {
         double longDifference = this.longitude - point.longitude;
         double latDifference = this.latitude - point.latitude;
-        double longDifSqure = Math.pow(longDifference,2);
-        double latDifSqure = Math.pow(latDifference,2);
+        double longDifSqure = Math.pow(longDifference, 2);
+        double latDifSqure = Math.pow(latDifference, 2);
         return (Math.sqrt(longDifSqure + latDifSqure));
     }
 
     /**
      * Check if this point is close to another point on map.
+     *
      * @param point another point on map.
      * @return true if the distance between is strictly less than 0.00015 degree.
      */
     public boolean closeTo(LongLat point) {
-        return (distanceTo(point) < Const.DTOLERANCE);
+        return (distanceTo(point) < Const.DISTANCE_TOLERANCE);
     }
 
     /**
      * Calculate the next position of drone given direction of its next move.
+     *
      * @param angle the direction of next move, 0 is East, 90 is North, 180 is West
      *              270 is South, the angle must be multiples of ten.
+     *              -999 means hovering (position doesn't change)
      * @return the next position.
      */
     public LongLat nextPosition(int angle) {
-        double newLong = this.longitude + 0.00015 * Math.cos(Math.toRadians(angle));
-        double newLat = this.latitude + 0.00015 * Math.sin(Math.toRadians(angle));
-        LongLat nextPos = new LongLat(newLong, newLat);
+        LongLat nextPos = new LongLat(this.longitude, this.latitude);
+        if (angle == Const.HOVERING) {
+            return (nextPos);
+        } else if (angle < 0 | angle > 350 | angle % 10 != 0) {
+            System.err.println("Angle must be multiples of ten " +
+                    "and between 0 and 350, or -999 for hovering");
+            return (nextPos);
+        }
+        // Cosine of angle to calculate vector of displacement parallel to longitude.
+        nextPos.longitude = this.longitude
+                + Const.DISTANCE_MOVE * Math.cos(Math.toRadians(angle));
+        // Sine of angle to calculate vector of displacement parallel to latitude.
+        nextPos.latitude = this.latitude
+                + Const.DISTANCE_MOVE * Math.sin(Math.toRadians(angle));
         return nextPos;
     }
 }
