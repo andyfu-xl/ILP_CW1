348e5d171d895c3c80f4756c9811be96d6feb302
andyfu-xl <somanyandyfus@gmail.com>
2021-10-26 15:06:06 +0100

    Deleted Menus and Apptest

--- src/main/java/uk/ac/ed/inf/Order.java
+++ src/main/java/uk/ac/ed/inf/Order.java
@@ -8,6 +8,7 @@ public class Order {
     private String customer;
     private String deliverTo;
     private ArrayList<String> items;
+    private int cost;
 
     public Order(String orderNo, String deliveryDate, String customer,
                  String deliverTo, ArrayList<String> items) {
@@ -18,9 +19,44 @@ public class Order {
         this.items = items;
     }
 
+    // getters
     public String getOrderNo() { return orderNo; }
     public String getDeliveryDate() { return deliveryDate; }
     public String getCustomer() { return customer; }
     public String getDeliverTo() { return deliverTo; }
     public ArrayList<String> getItem() { return items; }
+    public int getCost() { return cost; }
+
+    /**
+     *Calculate the cost in pence of all items delivered by drone.
+     *
+     * @param name name/IP of the server to connect with
+     * @param port port number of the server to connect with
+     */
+    public void calculateDeliveryCost(String name, String port) {
+        int foodCost = 0;
+        DataParser parser = new DataParser(name, port);
+        parser.readMenus();
+
+        /* for each items, we need to loop through all shops and their
+           menus to find the desired food item, then sum up the cost */
+        for (String foodItem : this.items) {
+            boolean found = false;      // Once the item is found, the loop will break.
+            for (Shop s : parser.getShops()) {
+                for (Item i : s.menu) {
+                    if (i.item.equals(foodItem)) {
+                        found = true;
+                        foodCost += i.pence;
+                        break;
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+        }
+        this.cost = foodCost + Const.DELIVERY_CHARGE;
+    }
+
+
 }
